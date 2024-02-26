8093d8cfb60cdf9b2ce04801eef5770005236a73
Emily Pulkowski <empulkowski@gmail.com>
2024-02-25 18:38:11 -0600

    initial Commit.

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '04e0b9c4fa98fea61d23');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '4cf18d14e2fff182dc8b');
